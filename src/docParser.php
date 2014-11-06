c35a084eceea3fbcf3c29df917c31017c1651f26
Sebastian Zartner <sebastianzartner@gmail.com>
2014-11-06 13:04:03 +0100

    Handled different cases parsing @return values in PHPDoc

--- src/docParser.php
+++ src/docParser.php
@@ -21,47 +21,54 @@ class DocParser {
   }
 
   private function parseReturn($returnTagString) {
-  	preg_match('/^@return\s+(\S+)(\s+(.*))?$/i', $returnTagString, $returnTagParts);
-  	return ['type' => $returnTagParts[1], 'desc' => $returnTagParts[2]];
+    preg_match('/^@return\s+(\S+)(\s+(.*))?$/i', $returnTagString, $returnTagParts);
+    switch (count($returnTagParts)) {
+      case 2:
+        return ['type' => '', 'desc' => $returnTagParts[1]];
+      case 3:
+        return ['type' => $returnTagParts[1], 'desc' => $returnTagParts[2]];
+      default:
+        return ['type' => '', 'desc' => ''];
+    }
   }
 
   function parse($docString) {
-     $strippedDocString = $this->stripJunk($docString);
-     $lines = preg_split('/\r\n|\r|\n/', $strippedDocString);
+    $strippedDocString = $this->stripJunk($docString);
+    $lines = preg_split('/\r\n|\r|\n/', $strippedDocString);
 
-     $i = 0;
-     do {
-       $this->doc['shortDesc'] .= $lines[$i];
-       $i++;
-     } while ($lines[$i] !== '' && $lines[$i][0] !== '@');
+    $i = 0;
+    do {
+      $this->doc['shortDesc'] .= $lines[$i];
+      $i++;
+    } while (!empty($lines[$i]) && isset($lines[$i][0]) && $lines[$i][0] !== '@');
 
-     while ($i < count($lines) && ($lines[$i] === '' || $lines[$i][0] !== '@')) {
-     	 $this->doc['longDesc'] .= $lines[$i];
-       $i++;
-     }
-     $this->doc['longDesc'] = trim($this->doc['longDesc']);
+    while ($i < count($lines) && ($lines[$i] === '' || $lines[$i][0] !== '@')) {
+      $this->doc['longDesc'] .= $lines[$i];
+      $i++;
+    }
+    $this->doc['longDesc'] = trim($this->doc['longDesc']);
 
-     while ($i < count($lines)) {
-     	 switch (preg_match('/^@\S+/', $lines[$i], $matches) === 1) {
-     	 	 case '@return':
-     	 	 	 $this->doc['tags']['return'] = $this->parseReturn($lines[$i]);
-     	 	 	 break;
-     	 }
+    while ($i < count($lines)) {
+      switch (preg_match('/^@\S+/', $lines[$i], $matches) === 1) {
+        case '@return':
+          $this->doc['tags']['return'] = $this->parseReturn($lines[$i]);
+          break;
+       }
        $i++;
      }
      $this->doc['rest'] = $lines;
   }
 
   function getTags() {
-  	return $this->doc['tags'];
+    return $this->doc['tags'];
   }
 
   function getTag($tagName) {
-  	foreach ($this->doc['tags'] as $tag => $tagParts) {
-  		if ($tag === $tagName)
-  			return $tagParts;
-  	}
-  	return [];
+    foreach ($this->doc['tags'] as $tag => $tagParts) {
+      if ($tag === $tagName)
+        return $tagParts;
+    }
+    return [];
   }
 
   function getShortDesc() {
@@ -69,11 +76,11 @@ class DocParser {
   }
 
   function getLongDesc() {
-  	return $this->doc['longDesc'];
+    return $this->doc['longDesc'];
   }
 
   function getDesc() {
-  	return $this->doc['shortDesc'] . chr(10) . chr(10) . $this->doc['longDesc'];
+    return $this->doc['shortDesc'] . chr(10) . chr(10) . $this->doc['longDesc'];
   }
 }
 ?>
